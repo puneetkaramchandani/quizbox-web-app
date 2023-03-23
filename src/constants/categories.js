320010bcd1aa58a9f532038b152a5eee9994d3e3
Puneet Karamchandani <jkaramchandani32@gmail.com>
2023-03-24 02:00:11 +0530

    added project constants

---
+++ src/constants/categories.js
@@ -0,0 +1,42 @@
+export const categories = [
+  {
+    name: 'Arts & Literature',
+    code: 'arts_and_literature',
+  },
+  {
+    name: 'Film & TV',
+    code: 'film_and_tv',
+  },
+  {
+    name: 'Food & Drink',
+    code: 'food_and_drink',
+  },
+  {
+    name: 'General Knowledge',
+    code: 'general_knowledge',
+  },
+  {
+    name: 'Geography',
+    code: 'geography',
+  },
+  {
+    name: 'History',
+    code: 'history',
+  },
+  {
+    name: 'Music',
+    code: 'music',
+  },
+  {
+    name: 'Science',
+    code: 'science',
+  },
+  {
+    name: 'Society & Culture',
+    code: 'society_and_culture',
+  },
+  {
+    name: 'Sport & Leisure',
+    code: 'sport_and_leisure',
+  },
+];
---
+++ src/constants/levels.js
@@ -0,0 +1,17 @@
+export const levels = [
+  {
+    name: 'Easy',
+    value: 'easy',
+    limit: 5,
+  },
+  {
+    name: 'Medium',
+    value: 'medium',
+    limit: 10,
+  },
+  {
+    name: 'Hard',
+    value: 'hard',
+    limit: 15,
+  },
+];
