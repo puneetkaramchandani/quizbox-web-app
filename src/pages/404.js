19471e1bdca49940b41ea688cb50cc355caf3436
Puneet Karamchandani <jkaramchandani32@gmail.com>
2023-03-24 02:00:45 +0530

    page not found page version 1

---
+++ src/pages/404.js
@@ -0,0 +1,19 @@
+import { Button, Result } from "antd";
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button href="/" type="primary">
+          Back Home
+        </Button>
+      }
+    />
+  );
+};
+
+export default PageNotFound;
